899007ed31e37fb9f5144442e58bad74ee0a6f1e
Federico Sgarella <fedex78@gmail.com>
2025-07-15 03:39:40 -0300

    222

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from.views import saludo
+from.views import saludo, saludo_con_template
 urlpatterns = [
     path("Hola mundo/", saludo),
+    path("hola-mundo-template/", saludo_con_template),
+
 ]
--- mi_primer_app/views.py
+++ mi_primer_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from views import saludo, saludo_con_template
 
 def saludo(request):
     return HttpResponse ("Hola Mundo")
+
+def saludo_con_template(request):
+    return render (request,'mi-primer-app/saludo.html')
